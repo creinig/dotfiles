130477bbd0a71eaa5dd4cdb9d66ca363e80cdbcf
Christian Reiniger <creinig@creinig.de>
2019-11-04 10:58:28 +0100

    Fixed OS recognition and package install

--- .local/bin/.df-common.sh
+++ .local/bin/.df-common.sh
@@ -4,7 +4,7 @@ if [[ $PREFIX =~ termux ]] ; then
 	DF_OS=termux
 elif grep -q rhel /etc/os-release 2>/dev/null ; then
 	DF_OS=rhel
-elif grep -q ubuntu /etc/lsb-release 2>/dev/null ; then
+elif grep -q ubuntu /etc/os-release 2>/dev/null ; then
 	DF_OS=ubuntu
 else
 	DF_OS=unknown
--- .local/bin/df-install-basics.sh
+++ .local/bin/df-install-basics.sh
@@ -2,25 +2,21 @@
 
 . $HOME/.local/bin/.df-common.sh
 
+PKG="     curl ntp"   i                     # Basic system stuff
+PKG="$PKG vim tmux git zsh unzip multitail" # basic convenience tools
+PKG="$PKG most bc pinfo tree mosh httpie"
+PKG="$PKG dstat sysstat htop atop iftop"    # System Info
+PKG="$PKG pigz lbzip2 parallel"             # parallel processing
 
 if [[ $DF_OS == 'termux' ]] ; then
 	pkg install mosh nodejs zsh vim multitail
 elif [[ $DF_OS == 'rhel' ]] ; then
-	sudo yum install \
-		bind-utils curl epel-release ntp # Basic system stuff \
-        vim tmux git zsh unzip multitail # basic convenience tools \
-		most bc pinfo tree mosh httpie \
-		dstat sysstat htop atop iftop    # System Info \
-		pigz lbzip2 parallel             # parallel processing
+	sudo yum install epel-release bind-utils
+	sudo yum install $PKG
 elif [[ $DF_OS == 'ubuntu' ]] ; then
-	sudo apt install \
-		bind-utils curl epel-release ntp # Basic system stuff \
-		dnsutils \
-        vim tmux git zsh unzip multitail # basic convenience tools \
-		most bc pinfo tree  mosh httpie \
-		dstat sysstat htop atop iftop    # System Info \
-		pigz lbzip2 parallel             # parallel processing
+	sudo apt install $PKG \
+		dnsutils
 else
-	echo "OS '$DS_OS' not supported"
+	echo "OS '$DF_OS' not supported"
 fi
 
