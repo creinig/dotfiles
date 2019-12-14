8f82a4e324e8323e7bf644ac063928377da8075f
Christian Reiniger <creinig@creinig.de>
2019-12-14 23:42:37 +0100

    install & alias for 'bat'

--- .local/bin/df-install-basics.sh
+++ .local/bin/df-install-basics.sh
@@ -2,14 +2,14 @@
 
 . $HOME/.local/bin/.df-common.sh
 
-PKG="     curl ntp"   i                     # Basic system stuff
+PKG="     curl ntp"                         # Basic system stuff
 PKG="$PKG vim tmux git zsh unzip multitail" # basic convenience tools
-PKG="$PKG most bc pinfo tree mosh httpie mosh"
+PKG="$PKG most bc pinfo tree mosh httpie bat"
 PKG="$PKG dstat sysstat htop atop iftop"    # System Info
 PKG="$PKG pigz lbzip2 parallel"             # parallel processing
 
 if [[ $DF_OS == 'termux' ]] ; then
-	pkg install mosh nodejs zsh vim multitail tree
+	pkg install mosh nodejs zsh vim multitail tree bat
 elif [[ $DF_OS == 'rhel' ]] ; then
 	sudo yum install epel-release bind-utils
 	sudo yum install $PKG
