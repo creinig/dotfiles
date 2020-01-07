37c5d389c43c3459506fa94f926cfef6213d1909
Christian Reiniger <creinig@creinig.de>
2020-01-07 08:33:07 +0100

    install: Fixed chsh call for multiple zsh options

--- .local/bin/df-install.sh
+++ .local/bin/df-install.sh
@@ -145,7 +145,7 @@ if ! command -v zsh >/dev/null ; then
 elif [[ ${SHELL##*/} != 'zsh' ]] && command -v zsh >/dev/null ; then
   logDo 'zsh is installed, but not the login shell'
   if [[ -f /etc/shells ]] ; then
-    chsh -s $(grep zsh /etc/shells)
+    chsh -s $(grep zsh /etc/shells | head -n 1)
     logDo '... fixed that'
   elif [[ $PREFIX =~ 'termux' ]] ; then
     chsh -s zsh
