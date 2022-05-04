8f7c36280da9a363009533a508f099879138cc78
Christian Reiniger <reiniger@weclapp.com>
2022-05-04 21:40:10 +0200

    devilspie: Fix reminders config

--- .config/devilspie2/reminders.lua
+++ .config/devilspie2/reminders.lua
@@ -1,9 +1,9 @@
 debug_print("Window Name: " .. get_window_name());
-debug_print("Application name: " .. get_application_name())
-debug_print("Window type: " .. get_window_type())
+debug_print("Application name: " .. get_application_name());
+debug_print("Window type: " .. get_window_type());
 
 if (string.find(get_window_name(), "Reminder") 
-    and (get_application_name() == "Thunderbird") 
-    and (get_window_type == "WINDOW_TYPE_DIALOG"))  then
+    and (get_application_name() == "Thunderbird"))  then
+    debug_print("  -> always on top");
     make_always_on_top()
 end
