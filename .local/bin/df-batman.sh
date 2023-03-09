4c775a5ebeb8305d8a432aeab64177f012e02dc0
Christian Reiniger <reiniger@weclapp.com>
2023-03-09 08:17:48 +0100

    sh: Fix bat/man integration

--- .local/bin/df-batman.sh
+++ .local/bin/df-batman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# Wrapper to ensure that the bat invocation also works with mandoc
-# (which ist the man implementation used in termux).
+# Wrapper around the bat invocation for mandoc
+# (which is the man implementation used in termux).
 # See https://github.com/sharkdp/bat/issues/1145
 #
 cat "$1" | col -bx | bat --language man --style plain
