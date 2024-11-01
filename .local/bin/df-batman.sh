0ec5c4ec2ff05942bf5877aa5b1ebae217e7c488
Christian Reiniger <creinig@creinig.de>
2024-11-01 21:20:59 +0100

    sh: df-batman.sh now handles stdin

--- .local/bin/df-batman.sh
+++ .local/bin/df-batman.sh
@@ -4,5 +4,9 @@
 # (which is the man implementation used in termux).
 # See https://github.com/sharkdp/bat/issues/1145
 #
-cat "$1" | col -bx | bat --language man --style plain
+if [ "x$1" = "x" ] ; then
+  col -bx | bat --language man --style plain
+else
+  cat "$1" | col -bx | bat --language man --style plain
+fi
 
