4d57c14f99761ca064b355c5aa250248d76c7d6b
Christian Reiniger <reiniger@weclapp.com>
2025-02-20 11:35:57 +0100

    nvim: add indent-blankline
    alias to "find register", but often more intuitive

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -27,6 +27,14 @@ return {
   },
 
   {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+
+  {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
