3c717b1b7fe8704dcc10b1b1c34f7484fdd5ec1c
Christian Reiniger <reiniger@weclapp.com>
2025-02-20 11:35:57 +0100

    nvim: improve telekasten mappings
    alias to "find register", but often more intuitive

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -71,28 +71,24 @@ return {
   {
     'renerocksai/telekasten.nvim',
     dependencies = {'nvim-telescope/telescope.nvim'},
-    config = function()
-      require('telekasten').setup({
-        home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
-      })
-
-      -- Launch panel if nothing is typed after <leader>w
-      vim.keymap.set("n", "<leader>w", "<cmd>Telekasten panel<CR>")
-
+    opts = {
+      home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
+    },
+    init = function()
       -- Most used functions
-      vim.keymap.set("n", "<leader>wf", "<cmd>Telekasten find_notes<CR>")
-      vim.keymap.set("n", "<leader>wg", "<cmd>Telekasten search_notes<CR>")
-      vim.keymap.set("n", "<leader>wt", "<cmd>Telekasten show_tags<CR>")
-      vim.keymap.set("n", "<leader>wj", "<cmd>Telekasten goto_today<CR>")
-      vim.keymap.set("n", "<leader>wz", "<cmd>Telekasten follow_link<CR>")
-      vim.keymap.set("n", "<leader>wn", "<cmd>Telekasten new_note<CR>")
-      vim.keymap.set("n", "<leader>wc", "<cmd>Telekasten show_calendar<CR>")
-      vim.keymap.set("n", "<leader>wb", "<cmd>Telekasten show_backlinks<CR>")
-      vim.keymap.set("n", "<leader>wI", "<cmd>Telekasten insert_img_link<CR>")
+      vim.keymap.set("n", "<leader>ww", "<cmd>Telekasten panel<CR>", { desc = "Toolbox" })
+      vim.keymap.set("n", "<leader>wf", "<cmd>Telekasten find_notes<CR>", { desc = "[F]ind notes by name"})
+      vim.keymap.set("n", "<leader>wg", "<cmd>Telekasten search_notes<CR>", { desc = "[G]rep in all notes"})
+      vim.keymap.set("n", "<leader>wt", "<cmd>Telekasten show_tags<CR>", { desc = "Look up by [T]ag"})
+      vim.keymap.set("n", "<leader>wj", "<cmd>Telekasten goto_today<CR>", { desc = "Open [J]ournal for today"})
+      vim.keymap.set("n", "<leader>wo", "<cmd>Telekasten follow_link<CR>", { desc = "[O]pen link"})
+      vim.keymap.set("n", "<leader>wn", "<cmd>Telekasten new_note<CR>", { desc = "[N]ew note"})
+      -- vim.keymap.set("n", "<leader>wc", "<cmd>Telekasten show_calendar<CR>")
+      vim.keymap.set("n", "<leader>wb", "<cmd>Telekasten show_backlinks<CR>", { desc = "Show [B]acklinks"})
+      -- vim.keymap.set("n", "<leader>wI", "<cmd>Telekasten insert_img_link<CR>")
 
       -- Call insert link automatically when we start typing a link
       vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
-
     end
   },
 
