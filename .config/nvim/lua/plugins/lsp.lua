9b343e008de2f516c7dca2b9794fd8ba6a646aa9
Christian Reiniger <reiniger@weclapp.com>
2025-02-20 11:35:57 +0100

    nvim: disable stylua (unavailable in teürmux)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -254,7 +254,8 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        -- stylua is not available in termux, resulting in startup errors
+        -- 'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
