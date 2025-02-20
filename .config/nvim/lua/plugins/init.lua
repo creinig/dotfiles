25862125141f062fe0ce1889dbad97eb0fe431d1
Christian Reiniger <reiniger@weclapp.com>
2025-02-20 11:35:57 +0100

    nvim: add telekasten
    alias to "find register", but often more intuitive

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -57,6 +57,34 @@ return {
     end
   },
 
+  {
+    'renerocksai/telekasten.nvim',
+    dependencies = {'nvim-telescope/telescope.nvim'},
+    config = function()
+      require('telekasten').setup({
+        home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
+      })
+
+      -- Launch panel if nothing is typed after <leader>w
+      vim.keymap.set("n", "<leader>w", "<cmd>Telekasten panel<CR>")
+
+      -- Most used functions
+      vim.keymap.set("n", "<leader>wf", "<cmd>Telekasten find_notes<CR>")
+      vim.keymap.set("n", "<leader>wg", "<cmd>Telekasten search_notes<CR>")
+      vim.keymap.set("n", "<leader>wt", "<cmd>Telekasten show_tags<CR>")
+      vim.keymap.set("n", "<leader>wj", "<cmd>Telekasten goto_today<CR>")
+      vim.keymap.set("n", "<leader>wz", "<cmd>Telekasten follow_link<CR>")
+      vim.keymap.set("n", "<leader>wn", "<cmd>Telekasten new_note<CR>")
+      vim.keymap.set("n", "<leader>wc", "<cmd>Telekasten show_calendar<CR>")
+      vim.keymap.set("n", "<leader>wb", "<cmd>Telekasten show_backlinks<CR>")
+      vim.keymap.set("n", "<leader>wI", "<cmd>Telekasten insert_img_link<CR>")
+
+      -- Call insert link automatically when we start typing a link
+      vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
+
+    end
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
