265bfec693fe1c75ed0d08fd03972f163e3ee0b6
Christian Reiniger <reiniger@weclapp.com>
2025-02-20 11:35:57 +0100

    nvim: add standardrb to autoinstall
    alias to "find register", but often more intuitive

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -47,7 +47,7 @@ return {
     cmd = { "ConformInfo" },
     keys = {
       {
-        "<leader>f",
+        "<leader>cf",
         function()
           require("conform").format({ async = true, lsp_format = "fallback" })
         end,
@@ -62,6 +62,7 @@ return {
         -- lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
+        ruby = { "standardrb" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -254,6 +254,7 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'standardrb',
         -- stylua is not available in termux, resulting in startup errors
         -- 'stylua', -- Used to format Lua code
       })
