722660f74ef30d6efd1c271ebe7d8360fa6100ee
Christian Reiniger <reiniger@weclapp.com>
2025-02-20 11:35:57 +0100

    nvim: Improve mappings for hop.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,10 +1,7 @@
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
   'jmcantrell/vim-diffchanges', -- diff changes to buffer since last write
-
   'AndrewRadev/linediff.vim', -- diff two visual selections
-
   'will133/vim-dirdiff', -- diff directory hierarchies
 
   { -- easier motion
@@ -18,18 +15,23 @@ return {
       require('hop').setup()
       local hop = require('hop')
       local directions = require('hop.hint').HintDirection
-      vim.keymap.set('', 'f', function()
+      vim.keymap.set('', '<leader>mf', function()
         hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-      end, {remap=true})
-      vim.keymap.set('', 'F', function()
+      end, {remap=true, desc = "Hop forward like [f]"})
+      vim.keymap.set('', '<leader>mF', function()
         hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-      end, {remap=true})
-      vim.keymap.set('', 't', function()
+      end, {remap=true, desc = "Hop backward like [F]"})
+      vim.keymap.set('', '<leader>mt', function()
         hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-      end, {remap=true})
-      vim.keymap.set('', 'T', function()
+      end, {remap=true, desc = "Hop forward [t]o char"})
+      vim.keymap.set('', '<leader>mT', function()
         hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-      end, {remap=true})
+      end, {remap=true, desc = "Hop backward [T]o char" })
+      vim.keymap.set('', '<leader>mw', function() hop.hint_words({ }) end, {remap=true, desc = "Hop to [w]ord"})
+      vim.keymap.set('', '<leader>mb', '<cmd>HopChar2<cr>' , {remap=true, desc = "Hop to [b]igram"})
+      vim.keymap.set('', '<leader>mp', '<cmd>HopPattern<cr>' , {remap=true, desc = "Hop to search [p]attern"})
+      vim.keymap.set('', '<leader>mv', '<cmd>HopVertical<cr>' , {remap=true, desc = "Hop [v]ertically"})
+      vim.keymap.set('', '<leader>ml', '<cmd>HopLineStart<cr>' , {remap=true, desc = "Hop to [l]ine"})
     end
   },
 
