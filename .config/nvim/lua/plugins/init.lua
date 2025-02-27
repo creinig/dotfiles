f22b3c86646d62652743c84c039389617a497711
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: move small plugins from ui to plugins.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -46,5 +46,38 @@ return {
     end
   },
 
+  {
+    "wtfox/jellybeans.nvim",
+    priority = 1000,
+    config = function()
+      require("jellybeans").setup()
+      vim.cmd.colorscheme("jellybeans")
+    end,
+  },
+
+  {
+    "szw/vim-maximizer",
+    keys = {
+      { "<F3>", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+    },
+  },
+
+  -- Highlight todo, notes, etc in comments
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = { signs = false },
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
 }
 
