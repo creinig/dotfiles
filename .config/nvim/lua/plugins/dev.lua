6c49a64454720f86d509528f2cf8f67ab5ebca58
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: move fugitive config to separate file

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -21,25 +21,6 @@ return {
     }
   },
 
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>gg", ":Git<cr>", { desc = "[G]it status" })
-      vim.keymap.set("n", "<leader>ga", ":Git add %", { desc = "[G]it [a]dd" })
-      vim.keymap.set("n", "<leader>gc", ":Git commit -v -c HEAD<cr>", { desc = "[G]it [commit] (prefill last msg)" })
-      vim.keymap.set("n", "<leader>gC", ":Git commit<cr>", { desc = "[G]it [c]ommit" })
-      vim.keymap.set("n", "<leader>gp", ":Git push<space>", { desc = "[G]it [p]ush" })
-      vim.keymap.set("n", "<leader>gf", ":Git fetch<space>", { desc = "[G]it [f]etch" })
-      vim.keymap.set("n", "<leader>gl", ":Git pull<space>", { desc = "[G]it pul[l]" })
-      vim.keymap.set("n", "<leader>gB", ":Gitsigns blame<cr>", { desc = "[G]it [b]lame" })
-      vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<cr>", { desc = "[G]it [b]lame (line)" })
-      vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<cr>", { desc = "[G]it [d]iff" })
-      vim.keymap.set("n", "<leader>gv", ":GV -n 1000<cr>", {desc = "Git [v]iew log" })
-      vim.keymap.set("v", "<leader>gv", ":GV -n 1000<cr>", {desc = "Git [v]iew log" })
-      vim.keymap.set("n", "<leader>gV", ":GV! -n 1000<cr>", {desc = "Git [v]iew log (this file)" })
-      -- TODO: add bindings for hunk operations
-    end
-  },
 
   { -- Autoformat
     "stevearc/conform.nvim",
