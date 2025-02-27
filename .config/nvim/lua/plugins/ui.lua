dd07f21dc2994d9b3c9498eb163cfe3e608f5d01
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: move lualine config to separate file

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -34,22 +34,4 @@ return {
     opts = {},
   },
 
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup {
-        options = {
-          icons_enabled = vim.g.have_nerd_font,
-          theme = 'auto',
-          component_separators = { left = '>', right = '<'},
-          section_separators = { left = '|', right = '|'},
-          always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = false,
-        },
-        extensions = {}
-      }
-    end
-  },
 }
