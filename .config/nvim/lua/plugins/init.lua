76379b65e2df97890ed884ce3786099aaa681ee1
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: move surround, easy-align config to main file

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -86,5 +86,25 @@ return {
     ---@type ibl.config
     opts = {},
   },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+
+  {
+    "junegunn/vim-easy-align",
+    config = function()
+      vim.keymap.set("x", "ga", "<Plug>(EasyAlign)", {desc = "EasyAlign"} )
+      vim.keymap.set("v", "ga", "<Plug>(EasyAlign)", {desc = "EasyAlign"} )
+      vim.keymap.set("n", "ga", "<Plug>(EasyAlign)", {desc = "EasyAlign"} )
+    end
+  },
 }
 
