f5aac2f3f70cc3aaf4b668d5a95a1bef1f0f5f79
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: auto-install shellcheck

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -255,6 +255,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'standardrb',
+        'shellcheck',
         -- stylua is not available in termux, resulting in startup errors
         -- 'stylua', -- Used to format Lua code
       })
