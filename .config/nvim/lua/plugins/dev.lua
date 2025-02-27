166a66236f5d400ab22e4c25954b908c2592b17c
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: move gitsigns config to separate file

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -1,19 +1,5 @@
 -- vim: ts=2:sw=2:et
 return {
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    },
-  },
-
   {
     'junegunn/gv.vim',
     dependencies = {
