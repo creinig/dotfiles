3c5cd5ec16c993734965c8d9c7f8d838127f6ff5
Christian Reiniger <creinig@creinig.de>
2025-02-27 19:08:34 +0100

    nvim: move conform config to separate file

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -21,36 +21,6 @@ return {
     }
   },
 
-
-  { -- Autoformat
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>cf",
-        function()
-          require("conform").format({ async = true, lsp_format = "fallback" })
-        end,
-        mode = "",
-        desc = "[F]ormat buffer",
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = nil,
-      formatters_by_ft = {
-        -- lua = { "stylua" },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        ruby = { "standardrb" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
-  },
-
   { -- Autocompletion
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
