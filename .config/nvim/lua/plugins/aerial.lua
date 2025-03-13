606ef662198da8df35337478690e5da8ad142947
Christian Reiniger <reiniger@weclapp.com>
2025-03-13 13:37:24 +0100

    nvim: tune aerial

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,14 +1,19 @@
 return {
   {
     'stevearc/aerial.nvim',
-    opts = {},
+    opts = {
+      nav = {
+        preview = true,
+      },
+    },
     -- Optional dependencies
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons"
     },
     init = function()
-      vim.keymap.set("n", "<leader>co", "<cmd>AerialToggle<cr>", { desc = "Toggle [O]utline" })
+      vim.keymap.set("n", "<leader>co", "<cmd>AerialNavToggle<cr>", { desc = "Toggle Quick [O]utline" })
+      vim.keymap.set("n", "<leader>cO", "<cmd>AerialToggle<cr>", { desc = "Toggle [O]utline" })
     end
   }
 }
