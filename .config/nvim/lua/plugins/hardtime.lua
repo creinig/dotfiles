3112946cb9bf4b1a206b4ee12761373b781c1b67
Christian Reiniger <reiniger@weclapp.com>
2025-03-13 13:37:24 +0100

    nvim: relax hardtime on repeats

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -3,6 +3,7 @@ return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {
+      max_count = 50,
       restriction_mode = "hint",
       disable_mouse = false,
       disabled_keys = {
