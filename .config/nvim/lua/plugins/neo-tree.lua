34898f72b1508ef0e2c270fa842184fe7d3c2b0d
Christian Reiniger <reiniger@weclapp.com>
2025-03-13 13:37:24 +0100

    nvim: auto-close neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,10 +8,25 @@ return {
       "MunifTanjim/nui.nvim",
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    -- init = function()
-    --   require("neo-tree").setup({
-    --     source_selector = { winbar = true }
-    --   })
-    -- end
+    init = function()
+      require("neo-tree").setup({
+        event_handlers = {
+
+          {
+            event = "file_open_requested",
+            handler = function()
+              -- auto close
+              -- vim.cmd("Neotree close")
+              -- OR
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+
+        }
+      })
+      --   require("neo-tree").setup({
+      --     source_selector = { winbar = true }
+      --   })
+    end
   }
 }
