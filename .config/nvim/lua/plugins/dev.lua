e7dc2008245cc585842dc3021ffc01bf4726d918
Christian Reiniger <reiniger@weclapp.com>
2025-03-17 08:38:27 +0100

    nvim: enable fuzzy matching in nvim-cmp

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -116,6 +116,9 @@ return {
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         }),
+        matching = {
+          disallow_fuzzy_matching = false,
+        },
         sources = {
           {
             name = "lazydev",
