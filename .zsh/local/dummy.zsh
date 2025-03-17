9beef11179cf6bf718d0ec854ed11afc50452380
Christian Reiniger <reiniger@weclapp.com>
2025-03-17 08:38:27 +0100

    zsh: add local/dummy.zsh

---
+++ .zsh/local/dummy.zsh
@@ -0,0 +1,5 @@
+# Placeholder file to (a) ensure the directory exists and (b) prevent the "no files found" warning
+#
+# Files (*.zsh) in this directory are intended for settings / aliases / ... that are specific to this
+# machine and should not be committed to git (or at least to the public repo).
+
