18671f2bebe1870b99ea134019739d048387f5d8
Christian Reiniger <reiniger@weclapp.com>
2025-03-17 08:38:27 +0100

    nvim: Add keymap to dismiss notifications

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -54,10 +54,12 @@ kms("n", "<leader>uw", "mw:%s/\\s\\+$//<cr>`w", { desc = "Remove all trailing wh
 
 kms("n", "<leader>ui", ":DiffChangesDiffToggle<cr>", { desc = "changes since last write" })
 kms("n", "<leader>ud", ":lcd %:p:h<Cr>:pwd<Cr>", { desc = 'cd (local) to current file' })
-kms("n", "<leader>un", ":Neotree<cr>", { desc = "Show [N]eotree" })
 kms("x", "<leader>us", ":sort<cr>", { desc = "Sort selection" })
 kms("n", "<leader>uS", "vip:sort<cr>", { desc = "Sort paragraph" })
 
+kms("n", "<leader>un", function()
+  require("notify").dismiss({ pending = false, silent = false })
+end, { desc = "Dismiss all [N]otifications" })
 
 -- [[Options]]
 
