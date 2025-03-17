5192b55ece1d90d96b338ee803bad7035d6cd254
Christian Reiniger <reiniger@weclapp.com>
2025-03-17 08:38:27 +0100

    nvim: stop syncing with system clipboard

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,14 +9,6 @@ vim.g.have_nerd_font = true
 
 vim.opt.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
-
 -- Indendation & Format
 vim.opt.autoindent    = true
 vim.opt.expandtab     = true
