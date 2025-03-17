04820897dfb16209a821b32987ec69db6bbb933d
Christian Reiniger <reiniger@weclapp.com>
2025-03-17 08:38:27 +0100

    nvim: Add keymap for Telescope notify

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -82,6 +82,7 @@ return {
       vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "[F]ind [r]egisters" })
       vim.keymap.set("n", "<leader>fy", builtin.registers, { desc = "[F]ind [y]ank history" })
       vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "[F]ind [M]arks" })
+      vim.keymap.set("n", "<leader>fn", ':Telescope notify<cr>', { desc = "[F]ind in [N]otifcations" })
       vim.keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "[F]ind [J]umps" })
       vim.keymap.set("n", "<leader>f:", builtin.command_history, { desc = "[F]ind [:] command history" })
       vim.keymap.set("n", "<leader>ft", builtin.current_buffer_tags, { desc = "[F]ind [T]ags in current buffer" })
