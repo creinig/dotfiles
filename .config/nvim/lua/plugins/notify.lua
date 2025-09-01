ac8e96b6a6aec9a8437445a7192d5c713f8320c9
Christian Reiniger <creinig@creinig.de>
2025-09-02 00:08:16 +0200

    nvim: disable notify in termux

--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -1,8 +1,11 @@
 return {
   {
     "rcarriga/nvim-notify",
+    -- in termux the popups hide too much content
+    cond = not string.find(vim.fn.stdpath("config"), "termux"),
     opts = {
       timeout = 10000,
+      stages = "static",
     }
   }
 }
